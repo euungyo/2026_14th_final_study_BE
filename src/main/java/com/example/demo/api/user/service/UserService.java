1098abb55a0d534d53cfb7701ff2366bfe59b632
Lee Eungyo <ialice119@naver.com>
2026-05-29 22:45:29 +0900

    refactor

--- src/main/java/com/example/demo/api/user/service/UserService.java
+++ src/main/java/com/example/demo/api/user/service/UserService.java
@@ -11,7 +11,6 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class UserService {
-
     private final UserRepository userRepository;
 
     public UserResponseDTO getMyInfo(Long userId) {
